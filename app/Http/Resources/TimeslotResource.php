bbe846572e5e0e9e9c7a65a7f4ef40075e14fd43
Alexandr Calganov <a.calganov@easyworx.nl>
2023-09-27 21:49:53 +0300

    fix/api - changed TimeslotResource return custom data to default

--- app/Http/Resources/TimeslotResource.php
+++ app/Http/Resources/TimeslotResource.php
@@ -8,12 +8,6 @@ class TimeslotResource extends JsonResource
 {
     public function toArray($request): array
     {
-        return [
-            'id' => $this->id,
-            'doctor_id' => $this->doctor_id,
-            'date' => $this->date,
-            'start_time' => $this->start_time,
-            'end_time' => $this->end_time,
-        ];
+        return parent::toArray($request);
     }
 }
